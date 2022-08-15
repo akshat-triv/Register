1cef89f57b80a5894567db838f4a3ac4a1e6243f
Akshat Trivedi <akshattriv.iit@gmail.com>
2022-08-15 11:59:27 +0530

    chore: added status bar and navigation bar plugin for capacitor js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from "./router";
 import store from "./store";
 
 import InlineSvg from "vue-inline-svg";
+import { StatusBar, Style } from "@capacitor/status-bar";
+import { NavigationBar } from "@hugotomazi/capacitor-navigation-bar";
+
+StatusBar.setStyle({ style: Style.Dark });
+StatusBar.setBackgroundColor({ color: "#0e1b3e" });
+
+NavigationBar.setColor({ color: "#0e1b3e" });
 
 const Vue = createApp(App);
 
